d358cc19d7c7e0fcce6ba40392d983b4ab59852c
Diego Alejandro Mora Suarez <dmora@gomezpinedaabogados.com>
2024-09-27 10:04:50 -0500

    codigo con sln de conexiones cerradas

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const excelJS = require('exceljs');
 const path = require('path');
 
 const app = express();
-const port = 4000;
+const port = 5000;
 
 app.use('/resources',express.static('public'));
 app.use('/resources',express.static(__dirname + 'public'));
@@ -13,16 +13,45 @@ app.use('/resources',express.static(__dirname + 'public'));
 
 // Configuración de la base de datos
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bd_inmobilariogpa'
+    host: '192.168.4.6',
+    user: 'desarrolloti',
+    password: 'd3cr3t05',
+    database: 'bd_inmobiliario',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 10000,
+    keepAliveInitialDelay: 10000
 });
 
 db.connect((err) => {
     if (err) throw err;
     console.log('Conectado a la base de datos MySQL');
 });
+
+// Habilitar keep-alive en el pool
+db.on('connection', function (connection) {
+    connection.ping();  // Envía un ping para mantener la conexión activa
+  });
+
+// Pool de conexiones para la base de datos
+db.on('error', function(err) {
+    console.log('Error en la base de datos:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      // En caso de pérdida de conexión, intenta reconectar
+      console.log('Conexión perdida. Intentando reconectar...');
+      pool.getConnection((error, connection) => {
+        if (error) {
+          console.error('Error al reconectar:', error);
+        } else {
+          console.log('Reconexión exitosa.');
+          connection.release();
+        }
+      });
+    } else {
+      throw err;  // Lanza otros errores
+    }
+  });
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,56 +66,56 @@ app.get('/', (req, res) => {
 
 // Ruta para buscar clientes por cédula
 app.post('/buscar', (req, res) => {
-    const cedula = req.body.cedula;
-    const sql = "SELECT *, DATE_FORMAT(fecha_asignacion,'%d-%b-%y') AS fecha FROM  gestion_comercial WHERE usuario_comercial = ?";
+    const numero_caso = req.body.numero_caso;
+    const sql = "SELECT *, DATE_FORMAT(fecha_asignacion,'%d-%b-%y') AS fecha FROM  gestion_comercial_dfvivienda WHERE numero_caso = ?";
     
-    db.query(sql, [cedula], (err, results) => {
+    db.query(sql, [numero_caso], (err, results) => {
         if (err) throw err;
         res.render('resultados', { clientes: results });
     });
 });
 
 // Ruta para exportar los datos a Excel
-app.get('/exportar/:cedula', (req, res) => {
-    const cedula = req.params.cedula;
-    const sql = 'SELECT * FROM gestion_comercial WHERE usuario_comercial = ?';
+// app.get('/exportar/:cedula', (req, res) => {
+//     const cedula = req.params.cedula;
+//     const sql = 'SELECT * FROM gestion_comercial WHERE usuario_comercial = ?';
 
-    db.query(sql, [cedula], async (err, results) => {
-        if (err) throw err;
+//     db.query(sql, [cedula], async (err, results) => {
+//         if (err) throw err;
 
-        const workbook = new excelJS.Workbook();
-        const worksheet = workbook.addWorksheet('Clientes');
+//         const workbook = new excelJS.Workbook();
+//         const worksheet = workbook.addWorksheet('Clientes');
 
-        worksheet.columns = [
-            { header: 'Cédula Cliente', key: 'cedula_cliente', width: 20 },
-            { header: 'Nombre Cliente', key: 'nombre_cliente', width: 30 },
-            { header: 'Fecha Asignación', key: 'fecha_asignacion', width: 20 },
-            { header: 'Número Caso', key: 'numero_caso', width: 20 },
-            { header: 'Valor Financiado', key: 'valor_financiado', width: 15 },
-            { header: 'Matrícula', key: 'matricula', width: 20 },
-            { header: 'Subclasificación', key: 'subclasificacion', width: 20 },
-            { header: 'Regional', key: 'regional', width: 20 },
-            { header: 'Responsable', key: 'responsable', width: 30 },
-            { header: 'Estado', key: 'estado', width: 20 },
-            { header: 'Comercial', key: 'comercial', width: 30 },
-            { header: 'Resumen Gestión', key: 'resumen_gestion', width: 40 }
-        ];
+//         worksheet.columns = [
+//             { header: 'Cédula Cliente', key: 'cedula_cliente', width: 20 },
+//             { header: 'Nombre Cliente', key: 'nombre_cliente', width: 30 },
+//             { header: 'Fecha Asignación', key: 'fecha_asignacion', width: 20 },
+//             { header: 'Número Caso', key: 'numero_caso', width: 20 },
+//             { header: 'Valor Financiado', key: 'valor_financiado', width: 15 },
+//             { header: 'Matrícula', key: 'matricula', width: 20 },
+//             { header: 'Subclasificación', key: 'subclasificacion', width: 20 },
+//             { header: 'Regional', key: 'regional', width: 20 },
+//             { header: 'Responsable', key: 'responsable', width: 30 },
+//             { header: 'Estado', key: 'estado', width: 20 },
+//             { header: 'Comercial', key: 'comercial', width: 30 },
+//             { header: 'Resumen Gestión', key: 'resumen_gestion', width: 40 }
+//         ];
 
-        worksheet.addRows(results);
+//         worksheet.addRows(results);
 
-        res.setHeader(
-            'Content-Type',
-            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        );
-        res.setHeader(
-            'Content-Disposition',
-            'attachment; filename=clientes.xlsx'
-        );
+//         res.setHeader(
+//             'Content-Type',
+//             'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+//         );
+//         res.setHeader(
+//             'Content-Disposition',
+//             'attachment; filename=clientes.xlsx'
+//         );
 
-        await workbook.xlsx.write(res);
-        res.end();
-    });
-});
+//         await workbook.xlsx.write(res);
+//         res.end();
+//     });
+// });
 
 
 app.listen(port, () => {
